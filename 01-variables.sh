3461e3474802e614d8dd972c8025f86757eac072
sainath <kollavenkatasai999@gmail.com>
2024-04-18 14:34:14 -0500

    script

---
+++ 01-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Mike
+
+echo "$PERSON1 : Hey $PERSON2, How are you doing?"
+echo "$PERSON2 : Hi $PERSON1, I am doing great. How are you doing?"
+echo "$PERSON1 : I am doing great too, How's your work going?"
+echo "$PERSON2 : Good!"
