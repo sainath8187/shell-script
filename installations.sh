65ed0cac7d89f43f0a89d5e061e823fed3487dd9
sainath <kollavenkatasai999@gmail.com>
2024-04-18 15:42:53 -0500

    script

--- installations.sh
+++ installations.sh
@@ -5,11 +5,18 @@ USERID=$(id -u)
 if [ $USERID -ne 0]
 then
     echo "Please run the script as root user"
-    exit 1
+    exit 4
 else
-    echo "script was running successfully"
+    echo "you are super user"
 fi
 
 dnf install mysql -y
 
-echo "script was running"
+if [ $? -ne 0]
+then
+    echo "Installation of mysql........Failure:
+    exit 4
+else
+    echo :Installation of mysql.......Success"
+fi
+
